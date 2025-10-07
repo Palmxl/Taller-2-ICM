464f668f18c124637986d20427fdbbb1483c1409
Natalia Ramirez <ramireznataa@gmail.com>
2025-10-07 10:52:00 -0500

    Fin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.taller2_icm"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.taller2_icm"
--- app/src/main/java/com/example/taller2_icm/MainActivity.kt
+++ app/src/main/java/com/example/taller2_icm/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.taller2_icm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // --- Configuración de los botones de navegación ---
+        val btnContactos = findViewById<ImageButton>(R.id.btnContactos)
+        val btnImagen = findViewById<ImageButton>(R.id.btnImagen)
+        val btnMapa = findViewById<ImageButton>(R.id.btnMapa)
+
+        // Ir a ContactosActivity
+        btnContactos.setOnClickListener {
+            startActivity(Intent(this, ContactosActivity::class.java))
+        }
+
+        // Ir a ImagenActivity
+        btnImagen.setOnClickListener {
+            startActivity(Intent(this, ImagenActivity::class.java))
+        }
+
+        // Ir a MapaActivity
+        btnMapa.setOnClickListener {
+            startActivity(Intent(this, MapaActivity::class.java))
         }
     }
 }
